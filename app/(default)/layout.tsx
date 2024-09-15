00038b51079b232dd855bf9d7ba668b045d8a054
Anthony Campos <anthonyrubencampos@gmail.com>
2024-09-15 02:51:28 -0400

    more changes

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -28,22 +28,22 @@ export default function DefaultLayout({
 
   const isDashboardRoute = pathname?.startsWith("/dashboard");
 
-  if (isDashboardRoute) {
-    return (
-      <div className="flex h-screen bg-gray-100">
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <div className="flex-shrink-0">
         <Sidebar
           expanded={sidebarExpanded}
           setExpanded={setSidebarExpanded}
         />
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <Header />
-          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
-            {children}
-          </main>
-        </div>
       </div>
-    );
-  }
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 
   return (
     <>
