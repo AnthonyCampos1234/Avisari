27ecde9f4a41946ff8959ddabb52557c7fa2a4f0
Anthony Campos <anthonyrubencampos@gmail.com>
2024-09-15 02:38:41 -0400

    more changes

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -8,58 +8,60 @@ export default function Header() {
   const { data: session, status } = useSession();
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-30 bg-gray-100">
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="flex h-16 items-center justify-between gap-3 rounded-b-2xl bg-white/70 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm">
-          {/* Site branding */}
-          <div className="flex flex-1 items-center">
-            <Logo />
-          </div>
+    <>
+      <header className="fixed top-2 z-30 w-full md:top-6">
+        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+          <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
+            {/* Site branding */}
+            <div className="flex flex-1 items-center">
+              <Logo />
+            </div>
 
-          {/* Desktop sign in links */}
-          <ul className="flex flex-1 items-center justify-end gap-3">
-            {status === "authenticated" ? (
-              <>
-                <li>
-                  <Link
-                    href="/dashboard"
-                    className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50"
-                  >
-                    Dashboard
-                  </Link>
-                </li>
-                <li>
-                  <button
-                    onClick={() => signOut()}
-                    className="btn-sm bg-gray-800 text-gray-200 shadow hover:bg-gray-900"
-                  >
-                    Sign Out
-                  </button>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link
-                    href="/signin"
-                    className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50"
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/signup"
-                    className="btn-sm bg-gray-800 text-gray-200 shadow hover:bg-gray-900"
-                  >
-                    Register
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
+            {/* Desktop sign in links */}
+            <ul className="flex flex-1 items-center justify-end gap-3">
+              {status === "authenticated" ? (
+                <>
+                  <li>
+                    <Link
+                      href="/dashboard"
+                      className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => signOut()}
+                      className="btn-sm bg-gray-800 text-gray-200 shadow hover:bg-gray-900"
+                    >
+                      Sign Out
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link
+                      href="/signin"
+                      className="btn-sm bg-white text-gray-800 shadow hover:bg-gray-50"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/signup"
+                      className="btn-sm bg-gray-800 text-gray-200 shadow hover:bg-gray-900"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
