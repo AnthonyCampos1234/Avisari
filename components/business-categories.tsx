e305e121a9266f499cbeabd54ab7360e66c5c1f9
Anthony Campos <anthonyrubencampos@gmail.com>
2024-09-15 14:53:33 -0400

    more changes

--- components/business-categories.tsx
+++ components/business-categories.tsx
@@ -19,31 +19,47 @@ export default function BusinessCategories() {
           </div>
           {/* Logos */}
           <div className="relative flex h-[324px] items-center justify-center">
-            {/* Replace the black glow with gradient animation */}
-            <div className="absolute inset-0 -z-10 overflow-hidden">
-              <div className="background">
-                <span style={{
-                  '--i': 1,
-                  left: '20%',
-                  top: '60%',
-                  animationDelay: '0s',
-                  animationDuration: '10s'
-                } as React.CSSProperties}></span>
-                <span style={{
-                  '--i': 2,
-                  left: '80%',
-                  top: '40%',
-                  animationDelay: '2s',
-                  animationDuration: '12s'
-                } as React.CSSProperties}></span>
-                <span style={{
-                  '--i': 3,
-                  left: '50%',
-                  top: '50%',
-                  animationDelay: '4s',
-                  animationDuration: '15s'
-                } as React.CSSProperties}></span>
-              </div>
+            {/* Gradient glow */}
+            <div className="absolute -z-10">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={432}
+                height={160}
+                viewBox="0 0 432 160"
+                fill="none"
+              >
+                <g opacity="0.6" filter="url(#filter0_f_2044_9)">
+                  <path
+                    className="fill-gradient"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M80 112C62.3269 112 48 97.6731 48 80C48 62.3269 62.3269 48 80 48C97.6731 48 171 62.3269 171 80C171 97.6731 97.6731 112 80 112ZM352 112C369.673 112 384 97.6731 384 80C384 62.3269 369.673 48 352 48C334.327 48 261 62.3269 261 80C261 97.6731 334.327 112 352 112Z"
+                  />
+                </g>
+                <defs>
+                  <filter
+                    id="filter0_f_2044_9"
+                    x={0}
+                    y={0}
+                    width={432}
+                    height={160}
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                  >
+                    <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="BackgroundImageFix"
+                      result="shape"
+                    />
+                    <feGaussianBlur
+                      stdDeviation={32}
+                      result="effect1_foregroundBlur_2044_9"
+                    />
+                  </filter>
+                </defs>
+              </svg>
             </div>
 
             {/* Small black dots */}
