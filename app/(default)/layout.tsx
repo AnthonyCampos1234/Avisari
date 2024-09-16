cde7e3a2d5bb236271afe184d8a2e58254150261
Anthony Campos <anthonyrubencampos@gmail.com>
2024-09-16 01:35:47 -0400

    more changes

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -38,9 +38,8 @@ export default function DefaultLayout({
         />
         <div className="flex-1 flex flex-col overflow-hidden">
           <header className="bg-white shadow-sm">
-            <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+            <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center">
               <h1 className="text-2xl font-semibold text-gray-900">Avisari</h1>
-              <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
             </div>
           </header>
           <main className={`flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 transition-all duration-500 ease-in-out ${sidebarExpanded ? "ml-48" : "ml-16"}`}>
