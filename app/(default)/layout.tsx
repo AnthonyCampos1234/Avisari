f014ed45482c10a6315495968db87f043fb5e135
Anthony Campos <anthonyrubencampos@gmail.com>
2024-09-16 01:37:57 -0400

    more changes

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -38,7 +38,7 @@ export default function DefaultLayout({
         />
         <div className="flex-1 flex flex-col overflow-hidden">
           <header className="bg-white shadow-sm">
-            <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center">
+            <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-center items-center">
               <h1 className="text-2xl font-semibold text-gray-900">Avisari</h1>
             </div>
           </header>
