9ff8e9614bf723610c42b83b94b86773c25c0d13
Anthony Campos <anthonyrubencampos@gmail.com>
2024-09-16 01:42:18 -0400

    more changes

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,41 +1,20 @@
 import Image from "next/image";
-import Stripes from "@/public/images/stripes-dark.svg";
 
 export default function Cta() {
   return (
     <section>
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div
-          className="relative overflow-hidden rounded-2xl text-center shadow-xl before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:rounded-2xl before:bg-gradient-to-r before:from-[#4ffbb4] before:via-[#f14771] before:to-[#23bbe9]"
+          className="relative overflow-hidden rounded-2xl text-center shadow-xl bg-gray-800"
           data-aos="zoom-y-out"
         >
-          {/* Glow */}
-          <div
-            className="absolute bottom-0 left-1/2 -z-10 -translate-x-1/2 translate-y-1/2"
-            aria-hidden="true"
-          >
-            <div className="h-56 w-[480px] rounded-full border-[20px] border-black blur-3xl" />
-          </div>
-          {/* Stripes illustration */}
-          <div
-            className="pointer-events-none absolute left-1/2 top-0 -z-10 -translate-x-1/2 transform"
-            aria-hidden="true"
-          >
-            <Image
-              className="max-w-none"
-              src={Stripes}
-              width={768}
-              height={432}
-              alt="Stripes"
-            />
-          </div>
           <div className="px-4 py-12 md:px-12 md:py-20">
-            <h2 className="mb-6 border-y text-3xl font-bold text-gray-200 [border-image:linear-gradient(to_right,transparent,theme(colors.slate.700/.7),transparent)1] md:mb-12 md:text-4xl">
-              Integrate Nota Solutions with your University
+            <h2 className="mb-6 text-3xl font-bold text-gray-200 md:mb-12 md:text-4xl">
+              Integrate Avisari Solutions with your University
             </h2>
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <a
-                className="btn group mb-4 w-full bg-gradient-to-t from-black to-gray-800 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
+                className="btn group mb-4 w-full bg-gray-700 text-white shadow hover:bg-gray-600 sm:mb-0 sm:w-auto"
                 href="#0"
               >
                 <span className="relative inline-flex items-center">
